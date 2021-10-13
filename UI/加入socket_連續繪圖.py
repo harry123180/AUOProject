e3a8405ba6b50907c5dbf8d3f17bcf0fd800896b
harry123180 <harry123180@gmail.com>
2021-10-14 00:34:52 +0800

    10.14

---
+++ UI/加入socket_連續繪圖.py
@@ -0,0 +1,225 @@
+# -*- coding: utf-8 -*-
+import PyQt5.QtWidgets
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtCore import QThread, pyqtSignal, QDateTime
+import pyqtgraph as pg
+import sys  # We need sys so that we can pass argv to QApplication
+from random import randint
+from ver1 import Ui_MainWindow
+import sys
+import socket
+from _thread import *
+
+
+
+class Mythread(QThread):
+    SENSOR_DATA = pyqtSignal(object)
+    ThreadCount = pyqtSignal(int)
+    print("成功開啟現成")
+    def __init__(self,*args, **kwargs):
+        super(Mythread, self).__init__()
+    # 重写QThread的run函数
+    def run(self):
+        ServerSocket = socket.socket()
+        host = '127.0.0.1'
+        port = 1233
+        ThreadCount = 0
+        try:
+            ServerSocket.bind((host, port))
+        except socket.error as e:
+            print(str(e))
+
+        print('Waitiing for a Connection..')
+        ServerSocket.listen(5)
+        self.ThreadCounts = 0
+        while True:
+            Client, address = ServerSocket.accept()
+            print('Connected to: ' + address[0] + ':' + str(address[1]))
+            start_new_thread(self.threaded_client, (Client,))
+            self.ThreadCounts += 1
+            self.ThreadCount.emit(self.ThreadCounts)#向LCD顯示函式發射目前現成連接數量
+            print('Thread Number: ' + str(ThreadCount))
+        ServerSocket.close()
+    def threaded_client(self,connection):
+        connection.send(str.encode('Welcome to the Servern'))
+        while True:
+            self.data = connection.recv(2048)
+            self.SENSOR_DATA.emit(self.data)#向GET DATA函式發射目前數據
+            self.reply = 'Server Says: ' + self.data.decode('utf-8')
+            if not self.data:
+                break
+            connection.sendall(str.encode(self.reply))
+        connection.close()
+
+
+
+class MySingnal(QThread):
+   # 信号创建记得放在__init__()之外，作为类属性
+   Data_Signal = QtCore.pyqtSignal(list)
+   Tip_Singal = QtCore.pyqtSignal(str,str)
+   Paint_Singal = QtCore.pyqtSignal(int)
+
+   def __init__(self):
+       super(MySingnal,self).__init__()
+
+   def Data_Sender(self,Data):
+       self.Data_Signal.emit(Data)
+
+   def Tip_Sender(self,title,message):
+       self.Tip_Singal.emit(title,message)
+
+   def Paint_Sender(self,int_data):
+       self.Paint_Singal.emit(int_data)
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        self.startstate = False#是否開始監控 初始化為否
+        self.connectstate = False#初始化連線狀態 為否
+        self.testmodestate = False#測試模式狀態
+        self.ThreadCount = 0 #感測器連線數量
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)#這邊的ui指的是我設計的ver1
+        #self.ui.toolButton_2.clicked.connect(self.start)
+        # Menu
+        self.ui.retranslateUi(self)
+        self.ui.actionClose.setShortcut('Ctrl+Q')
+        self.ui.actionClose.triggered.connect(app.exit)
+        self.ui.actionClose.setIcon(QtGui.QIcon('UI_Material/CloseIcon.png'))
+        # ToolBar
+        self.ui.toolButton.setShortcut('Ctrl+E')
+        self.ui.toolButton.setIcon(QtGui.QIcon('UI_Material/CloseIcon.png'))
+        self.ui.toolButton.clicked.connect(self.exit) #綁定觸發事件
+        #測試內網鈕
+        self.ui.toolButton_4.clicked.connect(self.test_mode)
+        #開始監控
+        self.ui.toolButton_2.clicked.connect(self.moniter)
+        #連線
+        self.ui.toolButton_3.clicked.connect(self.connect_socket)
+        #設定UI背景
+        image = QtGui.QPixmap()
+        image.load('UI_Material/BG.png')
+        image = image.scaled(self.width(), self.height())
+        palette = QtGui.QPalette()
+        palette.setBrush(self.backgroundRole(), QtGui.QBrush(image))
+        self.setPalette(palette)
+        #設定APP Icon
+        self.setWindowIcon(QtGui.QIcon('UI_Material/Icon.jpg'))
+        #宣告執行續
+        self.thread1= Mythread()
+        #Label2文字
+        self.ui.label_2.setStyleSheet("color:blue;background-color:white;")#給他框起來
+        self.ui.label_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.ui.label_2.setText("Normal State")
+        #初始化IP顯示
+        self.ip = self.get_host_ip()
+        #self.ui.label_4.setStyleSheet("color:black;background-color:white;")  # 給他框起來
+        self.ui.label_4.setText("當前連線IP = "+self.ip)
+        #lcd顯示線程數量
+        self.ui.lcdNumber.setDigitCount(12)
+        self.ui.lcdNumber.setMode(PyQt5.QtWidgets.QLCDNumber.Dec)
+        self.ui.lcdNumber.setStyleSheet("border: 2px solid black; color: red; background: silver;")
+        self.ui.lcdNumber.display(str(self.ThreadCount))
+        #ComboBox
+        #self.ui.comboBox.currentIndexChanged.connect(self.display)
+        #self.ui.comboBox.currentIndexChanged.connect(self.datasourse)
+        #Thread連結
+        # 宣告執行緒例項
+        self.ServerThread = Mythread()
+        #繫結SENSOR_DATA函式傳遞
+        self.ServerThread.SENSOR_DATA.connect(self.GET_DATA)
+        self.ServerThread.ThreadCount.connect(self.display_threadCount)
+
+
+        self.x = list(range(100))  # 100 time points
+        #self.y = [randint(0, 100) for _ in range(100)]  # 100 data points
+        self.y =list(0 for _ in range(100))
+        self.ui.graphWidget.setBackground('w')
+        self.ui.graphWidget.setTitle("test")
+        #self.ui.graphWidget.
+
+
+        pen = pg.mkPen(color=(255, 0, 0))
+        self.data_line = self.ui.graphWidget.plot(self.x, self.y, pen=pen)
+
+        # ... init continued ...
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(50)
+        self.timer.timeout.connect(self.update_plot_data)
+
+        global Gtestmodestate
+        Gtestmodestate = self.testmodestate
+    # def plot(self, hour, temperature):
+    #     self.ui.graphWidget.plot(hour, temperature)
+
+    def update_plot_data(self):
+        if(self.ui.comboBox_2.currentText() == "模擬數據"):
+            self.y = self.y[1:]  # Remove the first
+            self.y.append(randint(0, 100))  # Add a new random value.
+            self.data_line.setData(self.x, self.y)  # Update the data.
+        elif(self.ui.comboBox_2.currentText() == "清空數據"):
+            self.y =list(0 for _ in range(100))
+            self.data_line.setData(self.x, self.y)  # Update the data.
+        elif(self.ui.comboBox_2.currentText() == "真實數據"):
+            self.y = self.y[1:]  # Remove the first
+            self.y.append(randint(0, 100))  # Add a new random value.
+            self.data_line.setData(self.x, self.y)  # Update the data.
+
+    def exit(self):
+        app.exit()
+    def start(self):
+        self.thread1.start()
+    def connect_socket(self):
+        self.ui.label_2.setText("伺服器已開啟")
+        self.ui.label_2.setStyleSheet("color:red;background-color:white;")  # 給他框起來
+        self.server_thread()
+
+    def moniter(self):
+        if(self.startstate ==False):
+            print("開始監控",self.startstate)
+            self.ui.label_2.setText("Normal Monitoring")
+            self.ui.label_2.setStyleSheet("color:green;background-color:white;")  # 給他框起來
+            self.timer.start()
+            self.startstate = True
+        elif(self.startstate==True):
+            print("停止監控",self.startstate)
+            self.ui.label_2.setStyleSheet("color:red;background-color:white;")  # 給他框起來
+            self.ui.label_2.setText("STop  Monitoring")
+            self.timer.stop()
+            self.startstate =False
+
+    def get_host_ip(self):
+        try:
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.s.connect(('8.8.8.8', 80))
+            self.ip = self.s.getsockname()[0]
+        finally:
+            self.s.close()
+        return self.ip
+    def test_mode(self):
+        self.testmodestate = not self.testmodestate
+        self.ui.label_5.setText("內網模式"+str(self.testmodestate))
+        if(self.testmodestate == True):
+            self.ui.label_4.setText("當前連線IP = 127.0.0.1" )
+        else:
+            self.ip = self.get_host_ip()
+            self.ui.label_4.setText("當前連線IP = " + self.ip)
+
+
+
+    def server_thread(self):
+        print("成功進入serverThread")
+
+        self.ServerThread.start()
+    def GET_DATA(self,DATA):
+        self.Y_DATA = DATA
+        print("成功GET")
+    def display_threadCount(self,ThreadCount):
+        self.ui.lcdNumber.display(str(ThreadCount))
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    #app.setStyle(QStyleFactory.create("Windows"))
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
--- UI/可暫停開始_連續繪圖.py
+++ UI/可暫停開始_連續繪圖.py
@@ -40,7 +40,8 @@ class MySingnal(QThread):
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
-        self.startstate = False
+        self.startstate = False#是否開始監控 初始化為否
+        self.connectstate = False#初始化連線狀態 為否
         #super(MainWindow, self).__init__()
         super(MainWindow, self).__init__(*args, **kwargs)
         self.ui = Ui_MainWindow()
@@ -57,9 +58,11 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.toolButton.clicked.connect(self.exit) #綁定觸發事件
         #開始監控
         self.ui.toolButton_2.clicked.connect(self.moniter)
+        #連線
+        self.ui.toolButton_3.clicked.connect(self.connect_socket)
         #設定UI背景
         image = QtGui.QPixmap()
-        image.load('UI_Material/BG.jpg')
+        image.load('UI_Material/BG.png')
         image = image.scaled(self.width(), self.height())
         palette = QtGui.QPalette()
         palette.setBrush(self.backgroundRole(), QtGui.QBrush(image))
@@ -68,14 +71,18 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setWindowIcon(QtGui.QIcon('UI_Material/Icon.jpg'))
         #宣告執行續
         self.thread1= Mythread()
-
+        #Label2文字
+        self.ui.label_2.setStyleSheet("color:blue;background-color:white;")#給他框起來
+        self.ui.label_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.ui.label_2.setText("Normal State")
 
         self.x = list(range(100))  # 100 time points
         self.y = [randint(0, 100) for _ in range(100)]  # 100 data points
 
-        #self.graphWidget.setBackground('w')
+        self.ui.graphWidget.setBackground('w')
+        self.ui.graphWidget.setTitle("test")
+        #self.ui.graphWidget.
 
-        #self.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [30, 32, 34, 32, 33, 31, 29, 32, 35, 45])
 
         pen = pg.mkPen(color=(255, 0, 0))
         self.data_line = self.ui.graphWidget.plot(self.x, self.y, pen=pen)
@@ -90,8 +97,8 @@ class MainWindow(QtWidgets.QMainWindow):
     #     self.ui.graphWidget.plot(hour, temperature)
 
     def update_plot_data(self):
-        self.x = self.x[1:]  # Remove the first y element.
-        self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+        #self.x = self.x[1:]  # Remove the first y element.
+        #self.x.append(self.x[-1] + 0)  # Add a new value 1 higher than the last.
         self.y = self.y[1:]  # Remove the first
         self.y.append(randint(0, 100))  # Add a new random value.
         self.data_line.setData(self.x, self.y)  # Update the data.
@@ -100,15 +107,23 @@ class MainWindow(QtWidgets.QMainWindow):
         app.exit()
     def start(self):
         self.thread1.start()
+    def connect_socket(self):
+        self.ui.label_2.setText("Connect Failed")
+        self.ui.label_2.setStyleSheet("color:red;background-color:white;")  # 給他框起來
+
     def moniter(self):
         if(self.startstate ==False):
-            print("開始監控")
+            print("開始監控",self.startstate)
+            self.ui.label_2.setText("Normal Monitoring")
+            self.ui.label_2.setStyleSheet("color:green;background-color:white;")  # 給他框起來
             self.timer.start()
             self.startstate = True
         elif(self.startstate==True):
-            print("停止監控")
+            print("停止監控",self.startstate)
+            self.ui.label_2.setStyleSheet("color:red;background-color:white;")  # 給他框起來
+            self.ui.label_2.setText("STop  Monitoring")
             self.timer.stop()
-            self.startstate==False
+            self.startstate =False
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
